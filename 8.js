134a5c845f1726f5ee495888824056591dbb78ba
Ivan Sobenin <dn.sobenin75@gmail.com>
2024-07-24 20:32:28 +0300

    8

---
+++ 8.js
@@ -0,0 +1,11 @@
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+function invert(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] != 0) {
+      array[i] = array[i] * -1;
+    }
+  }
+
+  return array;
+}
