8f97469c6a85e7be08982c3f2cbc9c8e55c81c5b
Ivan Sobenin <dn.sobenin75@gmail.com>
2024-07-25 18:24:05 +0300

    18

---
+++ 18.js
@@ -0,0 +1,10 @@
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+function abbrevName(name) {
+  return name
+    .split(" ")
+    .map((part) => part[0].toUpperCase())
+    .join(".");
+}
