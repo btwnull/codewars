fbd5f8c16025a048f449c3722a7cf0b69018ae2c
Ivan Sobenin <dn.sobenin75@gmail.com>
2024-07-25 21:24:40 +0300

    25

---
+++ 25.js
@@ -0,0 +1,9 @@
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+function otherAngle(a, b) {
+  let thirdAngle = 180;
+  return thirdAngle - a - b;
+  return 0;
+}
